1fa58dc5393f0ba6d6c2e43db02374890212db08
Eunbyul <las4134@gmail.com>
2023-02-06 07:03:33 +0000

    add click event, update css

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+let menuItems = document.querySelectorAll("a");
+let length = menuItems.length;
+let currentLocation = location.href;
 
-let menuLists = document.querySelectorAll("li");
-
-menuLists.forEach( (li) => {
-  li.addEventListener("click", function () {
-    li.classList.add(".active");
-  })
+menuItems.forEach( (item) => {
+  if(currentLocation === item.href) {
+    item.parentElement.className = "active"
+    console.log(item.parentElement)
+  }
 })
