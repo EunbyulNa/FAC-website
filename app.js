f92fa08e521da0f0bdd6fd118d2b47cda10fecc6
Eunbyul <las4134@gmail.com>
2023-02-08 06:31:09 +0000

    add aboutme section,add chat function

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ let currentLocation = location.href;
 let titleImg = document.querySelector(".title-img");
 let titleDesc = document.querySelector(".title-desc");
 
+let chat = document.querySelector(".chat");
+let answer = document.querySelector(".answer");
+
 menuItems.forEach( (item) => {
   if(currentLocation === item.href) {
     item.parentElement.className = "active"
@@ -15,11 +18,23 @@ menuItems.forEach( (item) => {
 window.addEventListener("load", function() {
   titleImg.style.marginTop = 0;
   titleImg.style.opacity = '1';
-})
+});
 
 window.addEventListener("load", function () {
   setTimeout(function () {
     titleDesc.style.marginTop = 0;
     titleDesc.style.opacity = '1'
-  }, 1300)
-})
+  }, 1300);
+});
+
+window.addEventListener("load", function() {
+  chat.style.marginLeft= "30px";
+  chat.style.opacity = '1';
+});
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    answer.style.marginRight = "30px";
+    answer.style.opacity = '1'
+  }, 1300);
+});
