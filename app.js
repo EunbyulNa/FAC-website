542d4db6b49252cebbd6ff8b9d6028f2d121f593
Eunbyul <las4134@gmail.com>
2023-02-08 14:13:44 +0000

    Add about me chat function

--- app.js
+++ app.js
@@ -6,7 +6,23 @@ let titleImg = document.querySelector(".title-img");
 let titleDesc = document.querySelector(".title-desc");
 
 let chat = document.querySelector(".chat");
+let chatTwo = document.querySelector(".chatTwo");
+let chatThree = document.querySelector(".chatThree");
+let chatFour = document.querySelector(".chatFour");
+let chatFive = document.querySelector(".chatFive");
+
 let answer = document.querySelector(".answer");
+let answerTwo = document.querySelector(".answerTwo");
+let answerThree = document.querySelector(".answerThree");
+let answerFour = document.querySelector(".answerFour");
+let answerFive = document.querySelector(".answerFive");
+let answerSix = document.querySelector(".answerSix");
+let answerSeven = document.querySelector(".answerSeven");
+let answerEight = document.querySelector(".answerEight");
+let answerNine = document.querySelector(".answerNine");
+
+
+
 
 menuItems.forEach( (item) => {
   if(currentLocation === item.href) {
@@ -37,4 +53,45 @@ window.addEventListener("load", function () {
     answer.style.marginRight = "30px";
     answer.style.opacity = '1'
   }, 1300);
+});
+
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    chatTwo.style.marginLeft = "30px";
+    chatTwo.style.opacity = '1'
+  }, 4000);
+});
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    answerTwo.style.marginRight = "30px";
+    answerTwo.style.opacity = '1'
+    answerThree.style.marginRight = "30px";
+    answerThree.style.opacity = '1'
+    answerFour.style.marginRight = "30px";
+    answerFour.style.opacity = '1'
+  }, 5400);
+});
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    chatThree.style.marginLeft = "30px";
+    chatThree.style.opacity = '1';
+    answerFive.style.marginRight = "30px";
+    answerFive.style.opacity = '1';
+    answerSix.style.marginRight = "30px";
+    answerSix.style.opacity = '1';
+    chatFour.style.marginLeft = "30px";
+    chatFour.style.opacity = '1';
+    answerSeven.style.marginRight = "30px";
+    answerSeven.style.opacity = '1';
+    answerEight.style.marginRight = "30px";
+    answerEight.style.opacity = '1';
+    chatFive.style.marginLeft = "30px";
+    chatFive.style.opacity = '1';
+    answerNine.style.marginRight = "30px";
+    answerNine.style.opacity = '1';
+
+  }, 6400);
 });
